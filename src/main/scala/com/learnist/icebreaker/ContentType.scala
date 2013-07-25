fe5860b017583062dde89f1aed93dea5653963ee
Brandon Tilley <brandon@brandontilley.com>
2013-07-25 13:51:45 -0700

    Add default content types

---
+++ src/main/scala/com/learnist/icebreaker/ContentType.scala
@@ -0,0 +1,33 @@
+package com.learnist.icebreaker
+
+abstract class ContentType
+case object AmazonProduct extends ContentType
+case object BbcVideo extends ContentType
+case object DiscoveryVideo extends ContentType
+case object Flash extends ContentType
+case object GoogleBook extends ContentType
+case object GoogleDoc extends ContentType
+case object GoogleMap extends ContentType
+case object GrockitQuestion extends ContentType
+case object Html extends ContentType
+case object HuluVideo extends ContentType
+case object JustinTv extends ContentType
+case object Kickstarter extends ContentType
+case object KnowmiaLesson extends ContentType
+case object LearnistBoard extends ContentType
+case object MtvVideo extends ContentType
+case object NatGeoVideo extends ContentType
+case object Oembed extends ContentType
+case object OpenGraph extends ContentType
+case object Pdf extends ContentType
+case object Quizlet extends ContentType
+case object Scribd extends ContentType
+case object Soundcloud extends ContentType
+case object Spotify extends ContentType
+case object TedVideo extends ContentType
+case object Udemy extends ContentType
+case object UploadedImage extends ContentType
+case object Ustream extends ContentType
+case object VimeoVideo extends ContentType
+case object WikipediaArticle extends ContentType
+case object YoutubeVideo extends ContentType
