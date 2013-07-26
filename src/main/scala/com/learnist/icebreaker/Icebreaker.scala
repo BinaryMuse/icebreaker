aa16f9f43db0ef3f47b3cc1b4656930d06d24ca9
Brandon Tilley <brandon@brandontilley.com>
2013-07-26 12:35:29 -0700

    Oh hey functional programming

--- src/main/scala/com/learnist/icebreaker/Icebreaker.scala
+++ src/main/scala/com/learnist/icebreaker/Icebreaker.scala
@@ -1,23 +1,12 @@
 package com.learnist.icebreaker
 
 import scala.annotation.tailrec
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext
+import scala.concurrent._
 
 class Icebreaker(stack: Seq[Processor])(implicit context: ExecutionContext = ExecutionContext.Implicits.global) {
   def scrape(address: String): Future[Response] = {
     val request = Request(address)
-    val response = Future { Response(address, None, Nil, None, Map()) }
-    next(request, response, stack.toList)
-  }
-
-  @tailrec
-  private def next(request: Request, response: Future[Response], remaining: List[Processor]): Future[Response] = {
-    remaining match {
-      case processor :: rest =>
-        val resp = processor.process(request, response)
-        next(request, resp, rest)
-      case Nil => response
-    }
+    val response = future { Response(address, None, Nil, None, Map()) }
+    stack.foldLeft(response) { (resp, processor) => processor.process(request, resp) }
   }
 }
